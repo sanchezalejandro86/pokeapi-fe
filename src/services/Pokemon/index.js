288bbe06da11c4a3e70433eba0163d127a990739
Alejandro Sanchez <sanchezalejandro86@gmail.com>
2020-08-21 20:52:26 -0300

    change URL heroku

--- src/services/Pokemon/index.js
+++ src/services/Pokemon/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const urlPokeAPIMiddleware = "http://localhost:3000/pokemons";
-
+//const urlPokeAPIMiddleware = "http://localhost:3000/pokemons";
+const urlPokeAPIMiddleware = "https://pokeapi-middleware.herokuapp.com/pokemons";
 const PokemonService = {
 
     find: async function (name) {
